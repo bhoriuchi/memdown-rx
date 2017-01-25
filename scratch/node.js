efc2897f31a9aaf4655277532ded83ee9d042190
Branden Horiuchi <bhoriuchi@gmail.com>
2017-01-25 08:23:49 -0800

    adding test with rethinkdown

--- scratch/node.js
+++ scratch/node.js
@@ -1,5 +1,7 @@
 import Skiff from 'skiff'
 import memdown from 'memdown'
+import rethinkdbdash from 'rethinkdbdash'
+import rethinkdown from 'rethinkdown'
 import _ from 'lodash'
 import peerList from './peer-list'
 
@@ -8,7 +10,11 @@ export default function node (id) {
   let loc = _.first(_.filter(peerList, (peer) => peer.match(new RegExp(`${id}$`)) !== null))
   let peers = _.filter(peerList, (peer) => peer.match(new RegExp(`${id}$`)) === null)
 
-  const options = { db: memdown, peers }
+  const options = {
+    db: rethinkdown(rethinkdbdash, 'raft'),
+    location: 'raft',
+    peers
+  }
   const skiff = Skiff(loc, options)
   const db = skiff.levelup()
 
